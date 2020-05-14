127d99121ce47e5c2307b67b993d4a96c3a54b0c
Bruno Nunes <eubruno@me.com>
2020-05-14 14:06:05 -0300

    update mixin

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # moleculer-grpc-api
+
+[Moleculer gRPC API](https://grpc.io/) mixin for [Moleculer API Gateway](https://github.com/moleculerjs/moleculer-web)
+
+## Features
+
+## Install
+```
+npm i moleculer-grpc-api moleculer-web
+```
+
+## Usage
+This example demonstrates how to setup a Moleculer API Gateway with gRPC mixin in order to handle incoming gRPC requests.
+
+```js
+"use strict";
+
+const ApiGateway 	= require("moleculer-web");
+const { GrpcService } = require("moleculer-grpc-api");
+
+module.exports = {
+    name: "api",
+
+    mixins: [
+        // Gateway
+        ApiGateway,
+
+        // gRPC Server
+        GrpcService({
+
+            // Directory with you .proto files
+            directory: `${dirname}/../protos`,
+
+            // gRPC port. Default: 50051
+            port: ``
+
+            // List of actions available. ${protoPackage}.${protoService}/${serviceName}: ${moleculerService}.${moculerAction}
+            aliases: {
+                'helloworld.Greeter/sayHello': 'greeter.sayHello'
+            }
+        })
+    ]
+};
+
+```
+
+Start your Moleculer project and send gRPC requests.
+
+# Contribution
+Please send pull requests improving the usage and fixing bugs, improving documentation and providing better examples, or providing some testing, because these things are important.
+
+# License
+The project is available under the [MIT license](https://tldrlegal.com/license/mit-license).
--- src/service.js
+++ src/service.js
@@ -5,155 +5,87 @@ const grpc = require('grpc')
 const protoLoader = require('@grpc/proto-loader')
 
 module.exports = function(mixinOptions) {
+
     mixinOptions = _.defaultsDeep(mixinOptions, {
-        routeOptions: {
-            path: '/grpc'
-        },
+        port: 50051
     })
 
     const server = new grpc.Server()
 
     const serviceSchema = {
-        events: {
-            "$services.changed"() {
-                this.prepareGrpcServices()
-            }
-        },
         methods: {
 
-            invalidateGraphQLSchema() {
-			    this.shouldUpdateGraphqlSchema = true
-			},
-
             prepareGrpcServices() {
-                this.logger.info("♻ Preparing gRPC services")
-
                 try {
-                    const services = this.broker.registry.getServiceList({ withActions: true })
-                    this.addGrpcServices(services)
+                    const aliases = mixinOptions.aliases
+                    const directory = mixinOptions.directory
+                    this.logger.info(`♻ Preparing gRPC aliases ${directory}`)
+                    this.addGrpcServices(directory, aliases)
                 } catch (err) {
                     this.logger.error(err)
                     throw new Error(err)
                 }
             },
 
-            addGrpcServices(services) {
+            addGrpcServices(directory, aliases) {
+
                 try {
 
-                    const processedServices = new Set()
+                    for (const protoDef of Object.keys(aliases)) {
 
-                    services.forEach(service => {
-                        const serviceName = this.getServiceName(service)
-                        console.log('serviceName', serviceName)
+                        let actions = {}
 
-                        // Skip multiple instances of services
-						if (processedServices.has(serviceName)) return
-                        processedServices.add(serviceName)
+                        const action = aliases[protoDef]
 
-                        if (service.settings && service.settings.grpc) {
+                        const [protoFile, route] = protoDef.split('.')
+                        const [protoService, protoActionCall] = route.split('/')
 
-                            if (_.isObject(service.settings.grpc)) {
+                        this.logger.info(`gRPC ${protoFile}.${protoService}/${protoActionCall} => ${action}`)
 
-                                const def = service.settings.grpc
-
-                                let actions = {}
-                                Object.values(service.actions).forEach(action => {
-
-                                    const actionName = action.name.split('.')[1]
-
-                                    actions[actionName] = async (call, callback) => {
-
-                                        const {
-                                            params: staticParams = {},
-                                        } = def
-
-                                        try {
-                                            const params = call.request
-                                            const allParams = _.defaultsDeep({}, params, staticParams)
-
-                                            callback (null, await this.broker.call(
-                                                action.name,
-                                                _.defaultsDeep(allParams)
-                                            ))
-
-                                        } catch (err) {
-                                            callback(err)
-                                            throw new Error(err)
-                                        }
-
-                                    }
-
-                                })
-
-                                const PROTO_PATH = __dirname + `../../../../protos/${service.settings.grpc.proto}.proto`
-
-                                const packageDefinition = protoLoader.loadSync(PROTO_PATH)
-                                const proto = grpc.loadPackageDefinition(packageDefinition)[service.settings.grpc.proto]
-
-                                console.log(`service ${service.settings.grpc.service} added`, actions)
-                                try {
-                                    server.addService(proto[service.settings.grpc.service].service, actions)
-                                } catch (err) {
-                                    // console.log(err)
-                                }
-
+                        actions[protoActionCall] = async (call, callback) => {
+                            try {
+                                const params = call.request
+                                callback (null, await this.broker.call(
+                                    action,
+                                    params
+                                ))
+                            } catch (err) {
+                                callback(null, err)
+                                throw new Error(err)
                             }
-
                         }
 
-                    })
+                        const PROTO_PATH = `${directory}/${protoFile}.proto`
+                        const packageDefinition = protoLoader.loadSync(PROTO_PATH)
+                        const proto = grpc.loadPackageDefinition(packageDefinition)[protoFile]
+
+                        try {
+                            server.addService(proto[protoService].service, actions)
+                        } catch (err) {
+                            this.logger.error(err)
+                            throw new Error(err)
+                        }
+
+                    }
+
+                    this.startGrpcServer()
 
                 } catch (err) {
                     this.logger.error(err)
                     throw new Error(err)
                 }
-            },
-
-			getServiceName(service) {
-				if (service.fullName) return service.fullName;
-
-				if (service.version != null)
-					return (
-						(typeof service.version == "number"
-							? "v" + service.version
-							: service.version) +
-						"." +
-						service.name
-					);
-
-				return service.name;
             },
 
             startGrpcServer() {
-                server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+                server.bind(`0.0.0.0:${mixinOptions.port}`, grpc.ServerCredentials.createInsecure());
                 server.start()
-                this.logger.info(`🚀 gRPC server is available at ${mixinOptions.routeOptions.path}`)
-            }
+                this.logger.info(`🚀 gRPC server is available at ${mixinOptions.port}`)
+            },
+
         },
+
         created() {
-            const route = _.defaultsDeep(mixinOptions.routeOptions, {
-                aliases: {
-                    "/"(req, res) {
-                        try {
-                            this.startGrpcServer()
-                            return 'gRPC'
-                        } catch (err) {
-                            throw new Error(err)
-                        }
-                    }
-                },
-
-                mappingPolicy: "restrict",
-
-            })
-
-            // Add route
-            this.settings.routes.unshift(route)
-        },
-
-        started() {
-            if (process.env.NODE_ENV == "production") this.prepareGrpcServices()
-            this.logger.info(`🚀 gRPC server is available at ${mixinOptions.routeOptions.path}`)
+            this.prepareGrpcServices()
         },
     }
 
