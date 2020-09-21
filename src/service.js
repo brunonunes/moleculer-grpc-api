ab0496004232c2ac59f4b8636bfa15b61c21b677
trinhdaiphuc <trinhdaiphuc@gmail.com>
2020-09-21 20:26:27 +0700

    Error hendler for grpc service, update readme example

--- src/service.js
+++ src/service.js
@@ -4,6 +4,8 @@ const _ = require("lodash")
 const grpc = require('grpc')
 const protoLoader = require('@grpc/proto-loader')
 
+const { createError } = require("./utils")
+
 module.exports = function(mixinOptions) {
 
     mixinOptions = _.defaultsDeep(mixinOptions, {
@@ -54,7 +56,7 @@ module.exports = function(mixinOptions) {
                                     if (authentication) {
                                         const authenticationParams = authentication.params
                                         const [tokenParam] = Object.keys(authenticationParams)
-                                        const [data] = meta.get(authenticationParams[tokenParam])
+                                        const [data] = meta.get(tokenParam)
 
                                         let options = {}
                                         options[tokenParam] = data
@@ -68,9 +70,8 @@ module.exports = function(mixinOptions) {
                                         { meta }
                                     ))
                                 } catch (err) {
-                                    console.error(err)
-                                    callback(null, err)
-                                    throw new Error(err)
+                                    this.logger.error(err)
+                                    callback(createError(err.code, err.message, err.data), null)
                                 }
                             }
 
